d9ab053f16c00ca3d7fa64157f437e8344f6089a
sirdan <stanford303@gmail.com>
2022-08-04 16:57:13 -0700

    first topic done with test, controller, module, and sql

--- __tests__/beer.test.js
+++ __tests__/beer.test.js
@@ -4,18 +4,46 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('beer routes', () => {
-    beforeEach(() => {
-        return setup(pool);
-    });
+  beforeEach(() => {
+    return setup(pool);
+  });
 
-    it('/beer should show the list of beers', async () => {
-        const resp = await request(app).get('/beers');
-        expect(resp.body.length).toEqual(2);
-        const beer = resp.body.find((beers) => beers.id === '1');
-        expect(beers).toHaveProperty('name', 'type', 'pom'); 
-    })
+  it('/beer should show the list of beers', async () => {
+    const resp = await request(app).get('/beers');
+        
+    expect(resp.body).toEqual([
+      {
+        id: '1',
+        name: 'star dust',
+        type: 'IPA',
+        manufactured: '2020',
+
+      },
+      {
+        id: '2',
+        name: 'black hole',
+        type: 'stout',
+        manufactured: '2021',
+      },
+      {
+        id: '3',
+        name: 'pine ale',
+        type: 'IPA',
+        manufactured: '2022',
+      },
+      {
+        id: '4',
+        name: 'the kings mead',
+        type: 'IPA',
+        manufactured: '1775',
+      },
+
+    ]);
+  });
 
 
 
 
-})
+
+
+});
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,21 @@
--- Use this file to define your SQL tables
--- The SQL in this file will be executed when you run `npm run setup-db`
+Drop table if exists beer;
+
+
+
+
+
+
+
+CREATE table beers (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    type VARCHAR NOT NULL,
+    manufactured VARCHAR NOT NULL
+);
+
+
+INSERT INTO beers (name, type, manufactured) VALUES 
+('star dust', 'IPA', '2020'),
+('black hole', 'stout', '2021'),
+('pine ale', 'IPA', '2022'),
+('the kings mead', 'IPA', '1775');
